ebef885994bc774cf31b2a97ce86876c91903fcb
al-jisal <aljisal.frimpong@gmail.com>
2025-01-21 13:01:28 -0500

    added a UI structure

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,15 +3,23 @@
 #' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @import shinydashboard
 #' @noRd
 app_ui <- function(request) {
+  #################### HEADER ####################
+  header <- dashboardHeader()
+
+  #################### SIDEBAR ####################
+  sidebar <- dashboardSidebar()
+
+  #################### BODY ####################
+  body <- dashboardBody()
+
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
-    fluidPage(
-      golem::golem_welcome_page() # Remove this line to start building your UI
-    )
+    dashboardPage(header, sidebar, body)
   )
 }
 
