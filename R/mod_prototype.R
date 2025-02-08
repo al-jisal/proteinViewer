2b17633da283ed2d5b68cad2e4b95bedeee443ec
al-jisal <aljisal.frimpong@gmail.com>
2025-02-08 13:31:53 -0500

    the UI works perfectly

--- R/mod_prototype.R
+++ R/mod_prototype.R
@@ -1,3 +1,8 @@
+#################### LOAD DATA ####################
+path <- "data/LOAD2.ABCA7_normalizedabundance_withmetadata_proteomics.csv"
+data <- read.csv(path)
+proteins <- data[, 8:ncol(data)] # proteins are columns 8 to the end
+
 #' prototype UI Function
 #'
 #' @description this module is the first prototype of proteinViewer.
@@ -17,16 +22,14 @@ mod_prototype_ui <- function(id) {
 
   #################### SIDEBAR ####################
   sidebar <- dashboardSidebar(
-    # use varSelectInput when I have access to the data
-    # varSelectInput(inputId, label, data, selected = NULL, multiple = FALSE)
     # input area for Protein Selection
-    varSelectInput("protein", "Protein Name", c("A", "B", "C")),
+    varSelectInput("protein", "Protein Name", proteins),
     # input area for Sex selection
-    varSelectInput("sex", "Sex", c("male", "female"), multiple = TRUE),
+    selectInput("sex", "Sex", c("male", "female")),
     # input area for Age selection
-    varSelectInput("age", "Age", c("young", "old"), multiple = TRUE),
+    selectInput("age", "Age", c("4", "12", "18", "24")),
     # input area for Strain selection
-    varSelectInput("strain", "Strain", c("A", "B", "C"), multiple = TRUE),
+    selectInput("strain", "Strain", c("B6", "LOAD2", "LOAD2.ABCA7")),
     # button to update the plot when the user changes any input(s)
     actionButton("update", "Apply Changes",
                  icon("refresh"), class = "btn-success") # updates the plot
@@ -35,7 +38,6 @@ mod_prototype_ui <- function(id) {
   #################### BODY ####################
   body <- dashboardBody(
     h4("Filtered proteins:"),
-    uiOutput(ns("content"))
   )
 
   tagList(
